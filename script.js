cbe2cf8380485439c483ddf524fa55cf20cfb701
AidenCohen <acohen@sjnma.org>
2017-07-03 13:10:23 -0500

    CIRCLE FIX

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function (Circle) {
+function draw_circle() {
 var Draw_Circle = document.getElementByID("Aiden");
 var Circle_Dimensions = Draw_Circle.getContext("2d");
 Circle_Dimensions.beginPath();
 Circle_Dimensions.arc(100,50,20,0,2*Math.PI);
 Circle_Dimensions.stroke();
-}
+};
+
+draw_circle();
