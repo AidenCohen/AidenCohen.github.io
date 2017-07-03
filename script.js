be374e69744b63306658e5189a04919d824b8bb7
AidenCohen <acohen@sjnma.org>
2017-07-03 12:56:47 -0500

    Tweaked circle

---
+++ script.js
@@ -0,0 +1,5 @@
+var Draw_Circle = document.getElementByID("Aiden");
+var Circle_Dimensions = Draw_Circle.getContext("2d");
+Circle_Dimensions.beginPath();
+Circle_Dimensions.arc(100,50,20,0,2*Math.PI);
+Circle_Dimensions.stroke();
