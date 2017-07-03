97a606ae2d508f466681cb688a7e01db0cfc6697
AidenCohen <acohen@sjnma.org>
2017-07-03 13:19:14 -0500

    function changes

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-alert("im here!!!!!!!!!!");
+
 function draw_circle() {
 var Draw_Circle = document.getElementByID("Aiden");
 var Circle_Dimensions = Draw_Circle.getContext("2d");
 Circle_Dimensions.beginPath();
 Circle_Dimensions.arc(100,50,20,0,2*Math.PI);
 Circle_Dimensions.stroke();
+alert("im here!!!!!!!!!!");
 };
 
 draw_circle();
